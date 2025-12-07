8f11419c161924e3c08d6f25e642c094c2ee7f8d
Qualtagh <Qualtagh@users.noreply.github.com>
2025-12-07 20:18:42 +1300

    chore: fix config

--- jest.config.cjs
+++ jest.config.cjs
@@ -5,6 +5,7 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
   transform: {
     '^.+\\.tsx?$': ['ts-jest', {
+      tsconfig: 'tsconfig.test.json',
       diagnostics: {
         warnOnly: false,
       },
